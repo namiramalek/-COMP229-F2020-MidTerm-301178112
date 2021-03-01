2225b7390636718c3c6017230d94bff4dd32140a
Namira Malek <namiramalek234@gmail.com>
2021-03-01 16:28:38 +0530

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports =
+{
+    //"URI": "mongodb://localhost/book_store"
+   "URI": "mongodb+srv://namira:SWJBrKAd8AlSxvKV@databaseserver.9d4sb.mongodb.net/book_store?retryWrites=true&w=majority"
+ }
